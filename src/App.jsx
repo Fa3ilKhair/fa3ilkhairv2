568c16611b675cce27e63ce75c5bc7bd493dd781
khalidmarzou <khalidmarzoug@gmail.com>
2024-11-02 18:46:54 +0100

    transfer router to the main jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,17 @@ import Index from "./pages/Index";
 import NotFoundPage from "./pages/NotFoundPage";
 import { useTranslation } from "react-i18next";
 import { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 
-function MainContent() {
+export default function App() {
+  const { t, i18n } = useTranslation();
+
+  useEffect(() => {
+    document.documentElement.dir = i18n.language === "ar" ? "rtl" : "ltr";
+
+    document.title = i18n.language === "ar" ? "فاعل خير" : "Fa3il Khair";
+  }, [i18n.language]);
+
   const location = useLocation();
 
   const isNotFound = location.pathname !== "/fa3ilkhairv2/";
@@ -22,21 +30,5 @@ function MainContent() {
       </Routes>
       {!isNotFound && <Footer />}
     </>
-  );
-}
-
-export default function App() {
-  const { t, i18n } = useTranslation();
-
-  useEffect(() => {
-    document.documentElement.dir = i18n.language === "ar" ? "rtl" : "ltr";
-
-    document.title = i18n.language === "ar" ? "فاعل خير" : "Fa3il Khair";
-  }, [i18n.language]);
-
-  return (
-    <Router>
-      <MainContent />
-    </Router>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import "./css/index.css";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <I18nextProvider i18n={i18n}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </I18nextProvider>
   </StrictMode>
 );
